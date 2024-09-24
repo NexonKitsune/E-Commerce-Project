a92e8c24a76472071bd958f3085bb60ad1b6db43
ProPapers <KitsuneNeoxy@gmail.com>
2024-09-24 20:18:35 +0700

    React Update

---
+++ Website/Script/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to my React App</h1>
+      <p>Click the link below to visit the About Us page:</p>
+      <a href="/Website/index.html" target="_blank" rel="noopener noreferrer">Go to Website</a>
+    </div>
+  );
+}
+
+export default App;
